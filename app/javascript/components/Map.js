bbbc541e017b7e751e29933b7ea6dd2c9b948759
Rebecca Meritz <rebecca@meritz.com>
2020-03-29 17:10:45 -0700

    Diplay title on top of the map

--- app/javascript/components/Map.js
+++ app/javascript/components/Map.js
@@ -17,7 +17,14 @@ class Map extends React.Component {
   }
 
   render () {
-    return <div ref={el => this.mapContainer = el} />;
+    const style = {
+      position: 'absolute',
+      top: 0,
+      bottom: 0,
+      width: '100%'
+    };
+
+    return <div style={style} ref={el => this.mapContainer = el} />;
   }
 }
 
--- app/javascript/components/Title.js
+++ app/javascript/components/Title.js
@@ -3,7 +3,7 @@ import PropTypes from "prop-types"
 class Title extends React.Component {
   render () {
     return (
-      <h1>
+      <h1 className="title">
         Where in the world is {this.props.name}?
       </h1>
     );
